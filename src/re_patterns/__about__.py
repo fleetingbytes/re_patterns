25a1376c5f9e3cdac258970c9acbe151c54f6380
Sven Siegmund <sven.siegmund@iav.de>
2022-08-29 08:03:11 +0200

    it kinda works

--- src/re_patterns/__about__.py
+++ src/re_patterns/__about__.py
@@ -9,5 +9,5 @@ __author_email__ = "sven.siegmund@gmail.com"
 __maintainer__ = __author__
 __maintainer_email__ = __author_email__
 __release_date__ = date(year=2022, month=8, day=23)
-__version__ = "1.0.3"
+__version__ = "1.0.5"
 __repository__ = "https://github.com/Nagidal/re_patterns"
