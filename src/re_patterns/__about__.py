d27e68eb9a441833873881d8f9e3e006aab1a1cd
Sven Siegmund <sven.siegmund@iav.de>
2022-08-30 09:53:32 +0200

    Documentation is here

--- src/re_patterns/__about__.py
+++ src/re_patterns/__about__.py
@@ -8,5 +8,5 @@ __author_email__ = "sven.siegmund@gmail.com"
 __maintainer__ = __author__
 __maintainer_email__ = __author_email__
 __release_date__ = date(year=2022, month=8, day=23)
-__version__ = "1.0.8"
+__version__ = "1.1.0"
 __repository__ = "https://github.com/Nagidal/re_patterns"
